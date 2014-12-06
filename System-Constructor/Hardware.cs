6bf70b5c311fd249e0bbd81b9dd6921f14415aea
RodionPyanov <urodya@mail.ru>
2014-12-06 22:12:34 +0300

    files adding

---
+++ System-Constructor/Hardware.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_Constructor
+{
+    public class Processor
+    {
+        //some fields
+    }
+    public class VCard
+    {
+        //some fields
+    }
+    public class SCard
+    {
+        //some fields
+    }
+    public class Motherboard
+    {
+        //some fields
+    }
+    public class PowerUnit
+    {
+        //some fields
+    }
+    public class Cooler
+    {
+        //some fields
+    }
+    public class ROM
+    {
+        //some fields
+    }
+    public class HardDisk
+    {
+        //some fields
+    }
+}
