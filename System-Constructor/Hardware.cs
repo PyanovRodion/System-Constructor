701c50377f2f544556827f64f8b04fc8eeea260a
Alex <galckov.aleks@yandex.ru>
2014-12-07 23:14:33 +0400

    Class creation and Database changes

--- System-Constructor/Hardware.cs
+++ System-Constructor/Hardware.cs
@@ -8,34 +8,141 @@ namespace System_Constructor
 {
     public class Processor
     {
-        //some fields
+        public string Developer;
+        public string Name;
+        public int Cost;
+
+        public int Frequency;
+        public int NumberOfCores;
+        public string Socket;
+        public string Core;
+        public bool MultiplierAccess;
+        public int CashL1;
+        public int CashL2;
+        public int CashL3;
+        public int BusCash;
+        public int MultiplierNumber;
+        public int Temprature;
+
     }
     public class VCard
     {
-        //some fields
+        public string Developer;
+        public string Name;
+        public int Cost;
+
+        public string TypeOfCard;
+        public string TypeOfConnection;
+        public string ProcessorName;
+        public int NumberOfProcessors;
+        public int MemoryFrequency;
+        public int MemoryVolume;
+        public string MemoryType;
+        public int MonitorsConnected;
+        public int DVIOut;
+        public bool HDCP;
+        public int HDMIOut;
+        public string Resolution;
     }
     public class SCard
     {
-        //some fields
+        public string Developer;
+        public string Name;
+        public int Cost;
+
+        public string Type;
+        public string ConnectionType;
+        public bool AdditionalPower;
+        public int FrequencyACP;
+        public int FrequencyCAP;
+        public int MAXFrequencyCAP;
+        public int InChanelsNumber;
     }
     public class Motherboard
     {
-        //some fields
+        public string Developer;
+        public string Name;
+        public int Cost;
+
+        public string FormFactor;
+        public string Socket;
+        public string Chipset;
+        public bool MultiProcessor;
+        public string BIOS;
+        public string CrossFire;
+        public string SLI;
+        public string MemoryType;
+        public int MAXMemoryVolume;
+        public string SATA;
+        public string Sounds;
+        public string Ethernet;
+        public int PCINumber;
+        public int PCIExpressNumber;
+        public int USB;
+        public int HDMI;
+        public bool DVI;
     }
     public class PowerUnit
     {
-        //some fields
+        public string Developer;
+        public string Name;
+        public int Cost;
+
+        public int Power;
+        public string Sertificate80Plus ;
+        public int CoolerSystem;
+        public int CoolerSize;
+        public int SeconderyCoolerSize;
+        public string PFS;
+        public string ATX12VVersion;
+        public string MotherboardConnectionType;
+        public bool SpecialCables;
+        public bool Surge;
+        public bool Overload;
+        public bool ShortCircuit;
     }
     public class Cooler
     {
-        //some fields
+        public string Developer;
+        public string Name;
+        public int Cost;
+
+        public string Usage;
+        public bool WaterCool;
+        public int NumberOfFans;
+        public int FansSize;
+        public string Socket;
     }
     public class ROM
     {
-        //some fields
+        public string Developer;
+        public string Name;
+        public int Cost;
+
+        public string Type;
+        public string FormFactor;
+        public int NumberOfBlocks;
+        public int BlockVolume;
     }
     public class HardDisk
     {
-        //some fields
+        public string Developer;
+        public string Name;
+        public int Cost;
+
+        public int FormFactor;
+        public int Volume;
+        public string Type;
+        public int SATAConnection;
+        public bool IDE;
+        public bool PCIEConnection;
+        public int PCIType;
+        public bool ZIF40Pin;
+        public bool eSATA;
+        public bool mSATA;
+        public int SpinSpeed;
+        public int ReadSpeed;
+        public int WriteSpeed;
+        public int BuferVolume;
     }
 }
