d045b57fc4f5f029e4606146ba42a69dec3fe76a
olegnemch <docnem@rambler.ru>
2014-12-07 14:55:08 +0400

    AllTablesHaveFields oNem

--- System-Constructor/Form1.cs
+++ System-Constructor/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace System_Constructor
 {
@@ -16,6 +17,24 @@ namespace System_Constructor
         {
 
             InitializeComponent();
+            Database_Computer_PartsDataSet dbc=new Database_Computer_PartsDataSet();
+
+            DataTable ProcTable= dbc.Процессоры;
+            DataTable BlockpitTable = dbc.Блоки_питания;
+            DataTable ZvukKartaTable = dbc.Звуковые_карты;
+            DataTable VideoKardsTable = dbc.Видео_карты;
+            DataTable HarddrivesTable = dbc.Жесткие_диски_и_сетевые_накопители;
+            DataTable CoolerTable = dbc.Кулеры_и_системы_охлаждения;
+            DataTable MotheRSTable = dbc.Материнские_платы;
+
+            dataGridViewProcessor.DataSource = ProcTable;
+            dataGridViewMotherboard.DataSource = MotheRSTable;
+            dataGridViewCooler.DataSource=CoolerTable;
+            dataGridViewHardDisk.DataSource = HarddrivesTable;
+            dataGridViewPowerUnit.DataSource = BlockpitTable;
+            dataGridViewSCard.DataSource = ZvukKartaTable;
+            dataGridViewVCard.DataSource = VideoKardsTable;
+
         }
 
         private void buttonSelectProc_Click(object sender, EventArgs e)
